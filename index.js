f4de0b3118aae845a6872959cd85b52f81e9eb6f
Jacob Levine <Jacob.levine93@protonmail.com>
2018-10-30 18:50:16 -0700

    Node Core Libraries Complete

---
+++ index.js
@@ -0,0 +1,23 @@
+const http = require('http');
+const port = 3000;
+
+const requestHandler = (request, response) => {
+
+const fs = require('fs');
+
+fs.appendFileSync('hello-word.txt', "Hello to this great world");
+
+	response.end(`Handling a request on port ${port}`)
+};
+
+const server = http.createServer(requestHandler);
+
+server.listen(port, (err) => { 
+  if (err) { 
+    return console.log(`You have an error: ${err}`); 
+  }
+  console.log(`server is listening on port ${port}`) 
+}); 
+
+
+
